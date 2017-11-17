29ea8b2a8452f6840ca569c3fad893a2aaa7e4e7
tanaka ken <tanaka-ken@netlab.jp>
2017-11-17 11:45:10 +0900

    to enable browser-select(1)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require("./lib/page_walker");
+const pageWalker = module.exports = require("./lib/page_walker");
+
+if(process.argv[1].match(/\/pagewalker\/index.js$/)){
+  const Config = require("./lib/utils/config");
+  pageWalker.start(Config.create(process.argv.slice(2)));
+}
