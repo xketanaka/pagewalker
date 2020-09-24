fb6429fd1c3a943ea63a2ebaa75afbfc0db134b2
tanaka ken <tanaka-ken@netlab.jp>
2020-09-24 11:36:59 +0900

    Make importable as module and Delete fs_ext.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 global.Promise = require('bluebird');
 global.Promise.config({ longStackTraces: true });
 
+const Config = require("./lib/utils/config");
+
 function main(config){
   if(!config){
-    const Config = require("./lib/utils/config");
     config = Config.create(process.argv.slice(2));
   }
 
@@ -11,9 +12,16 @@ function main(config){
   pageWalker.start(config);
 }
 
-if(process.argv[1].match(/\/pagewalker\/index.js$/)){
+if(process.argv[1] && process.argv[1].match(/\/pagewalker\/index.js$/)){
   // launch with electron
   main();
 }
-
-module.exports = main;
+else if(process.argv[1] && process.argv[1].match(/pagewalker$/)){
+  // launch with puppeteer
+  module.exports = main;
+}
+else{
+  config = Config.create([]);
+  const pageWalker = module.exports = require("./lib/page_walker");
+  pageWalker.initialize(config);
+}
