48523040b9c138a91b72ebdf358d89caa6fe54e0
tanaka ken <tanaka-ken@netlab.jp>
2025-09-29 18:07:51 +0900

    Updating 01_sample

--- example/01_sample_scenario.js
+++ example/01_sample_scenario.js
@@ -3,21 +3,30 @@ const assert = require('assert');
 
 describe('First example', ()=>{
 
-  it('Load page', async ()=>{
+  it('Visit Github and Inspect code', async function(){
 
-    await page.load('https://www.google.com')
+    await page.load('https://github.com/xketanaka/pagewalker');
 
-    await page.find('textarea[name=q]').setValue('pagewalker');
+    await assert.strictEqual(page.url, 'https://github.com/xketanaka/pagewalker');
 
-    await page.find('input[type=submit]').haveValue('Google 検索').click();
+    await page.find('div.search-input-container button').click();
 
-    await page.waitForPageLoad();
+    await page.find('input#query-builder-test').fillIn("repo:xketanaka/pagewalker 01_sample_scenario.js");
 
-    await page.find('a h3').haveText('xketanaka/pagewalker').click();
+    await page.find('input#query-builder-test').submit();
 
-    await page.waitForPageLoad();
+    await page.waitForFinder(page.find("h2#search-filters-title").haveText("Filter by"));
 
-    await assert.equal(page.url, 'https://github.com/xketanaka/pagewalker');
+    await page.find('a').textIncludes("Updating 01_sample_scenario").click();
+
+    await page.waitForFinder(page.find("h1").textIncludes("Updating 01_sample_scenario.js"));
+
+    await page.find("div#issue-body-viewer a").haveText("01_sample_scenario").click();
+
+    await page.waitForSelector("textarea#read-only-cursor-text-area");
+
+    const expected = 'We have verified that this text exists.';
+    assert(await page.find("textarea#read-only-cursor-text-area").textIncludes(expected).exist());
   });
 
 });
